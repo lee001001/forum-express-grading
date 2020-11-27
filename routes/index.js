4b68e6eaacd8e3d382d2435f192476b4c85e392f
King LEE <a0988698776@gmail.com>
2020-11-27 22:47:29 +0800

    Update admin create page for editing usage & Install method-override & Add admin restaurant put feature

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,10 @@ module.exports = (app, passport) => {
 
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
 
+  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
+
+  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 
