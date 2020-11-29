cda5719ff3d4510dbf439a147bd573a07299f757
King LEE <a0988698776@gmail.com>
2020-11-29 13:48:14 +0800

    fetch:Install Imgur api and update admin POST and PUT & Add dotenv

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', handlebars())
