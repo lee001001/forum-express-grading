de1032d2cb2c9bdfbdc18213adf4d8dd92477f2e
King LEE <a0988698776@gmail.com>
2020-12-12 01:14:59 +0800

    fetch:DELETE /api/admin/categories/:id - 後台刪除類別

--- services/categoryService.js
+++ services/categoryService.js
@@ -43,6 +43,15 @@ const categoryService = {
             })
         })
     }
+  },
+  deleteCategory: (req, res, callback) => {
+    return Category.findByPk(req.params.id)
+      .then((category) => {
+        category.destroy()
+          .then((category) => {
+            callback({ status: 'success', message: '' })
+          })
+      })
   }
 }
 module.exports = categoryService
